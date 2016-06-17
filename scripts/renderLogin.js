72602a57cd001958c90e54eb6997737cccd52a49
camilin87 <camilin87@gmail.com>
2016-06-17 08:32:48 -0400

    minor refactor to extract the url params reading out of the login button

--- scripts/renderLogin.js
+++ scripts/renderLogin.js
@@ -1,39 +1,42 @@
-var LoginButton = React.createClass({
-    getInitialState: function() {
-        function getQueryStringParams(sParam){
-            var sPageURL = window.location.search.substring(1);
-            var sURLVariables = sPageURL.split('&');
+(function(){
+    function getQueryStringParams(sParam){
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
 
-            for (var i = 0; i < sURLVariables.length; i++){
-                var sParameterName = sURLVariables[i].split('=');
-                if (sParameterName[0] == sParam){
-                    return sParameterName[1];
-                }
+        for (var i = 0; i < sURLVariables.length; i++){
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam){
+                return sParameterName[1];
             }
         }
-        var isDev = getQueryStringParams("dev") || null;
-
-        var baseUrl = "https://twu-loginapi.herokuapp.com";
-        if (isDev === "true"){
-            baseUrl = "http://localhost:8080";
-        }
-
-        var loginApiUrl = baseUrl + "/api/v1/login";
-
-        return {
-            url: loginApiUrl + "?callback=" + window.location.href
-        }
-    },
-    render: function() {
-        return (
-            <a className="btn btn-lg btn-success" role="button" href={this.state.url}>
-                Signup Today
-            </a>
-        );
     }
-});
 
-ReactDOM.render(
-    <LoginButton/>,
-    document.getElementById("loginContainer")
-);
+    var LoginButton = React.createClass({
+        getInitialState: function() {            
+            var isDev = getQueryStringParams("dev") || null;
+
+            var baseUrl = "https://twu-loginapi.herokuapp.com";
+            if (isDev === "true"){
+                baseUrl = "http://localhost:8080";
+            }
+
+            var loginApiUrl = baseUrl + "/api/v1/login";
+
+            return {
+                url: loginApiUrl + "?callback=" + window.location.href
+            }
+        },
+        render: function() {
+            return (
+                <a className="btn btn-lg btn-success" role="button" href={this.state.url}>
+                    Signup Today
+                </a>
+            );
+        }
+    });
+
+    ReactDOM.render(
+        <LoginButton/>,
+        document.getElementById("loginContainer")
+    );
+})();
