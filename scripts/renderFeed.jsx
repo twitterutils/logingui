fc0ce7750d53330ade9ac2f2db358493905f24ce
camilin87 <camilin87@gmail.com>
2016-07-30 07:34:44 -0400

    start building a reactive feed

---
+++ scripts/renderFeed.jsx
@@ -0,0 +1,16 @@
+(function(){
+    var Feed = React.createClass({
+        render: function(){
+            return (
+                <div>
+                    the feed
+                </div>
+            )
+        }
+    })
+
+    ReactDOM.render(
+        <Feed />,
+        document.getElementById("feed-container")
+    )
+})();
