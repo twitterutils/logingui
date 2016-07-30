ed24ae4e379166522e786b42543b038ace75849c
camilin87 <camilin87@gmail.com>
2016-07-30 07:58:14 -0400

    define a very basic feed rendering infrastructure

--- scripts/renderFeed.jsx
+++ scripts/renderFeed.jsx
@@ -1,16 +1,59 @@
 (function(){
-    var Feed = React.createClass({
+    var FeedItems = React.createClass({
+        render: function(){
+            return (
+                <ul>
+                    {(this.props.items || []).map(function (i){
+                        return <SingleFeedItem key={i} item={i} />;
+                    })}
+                </ul>
+            )
+        }
+    });
+
+    var SingleFeedItem = React.createClass({
+        render: function(){
+            return (
+                <li>
+                    {this.props.item}
+                </li>
+            )
+        }
+    });
+
+    var Loading = React.createClass({
+        render: function(){
+            return (
+                <div id="feed-loading">Loading Feed...</div>
+            )
+        }
+    });
+
+    var FeedError = React.createClass({
         render: function(){
             return (
                 <div>
-                    the feed
+                    <h2>Error Loading Feed</h2>
+                    <p>{this.props.error}</p>
                 </div>
             )
         }
-    })
+    });
+
+    var FeedContainer = React.createClass({
+        render: function(){
+            return (
+                <div>
+                    <Loading />
+                    <FeedError error="something went wrong" />
+                    <FeedItems items={[1, 3, 4, 5]}/>
+                </div>
+            )
+        }
+    });
 
     ReactDOM.render(
-        <Feed />,
+        <FeedContainer />,
         document.getElementById("feed-container")
-    )
+    );
 })();
