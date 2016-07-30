0386b4cf81dfc2a955b86a54611be062598759ea
camilin87 <camilin87@gmail.com>
2016-07-30 08:23:05 -0400

    read the username from the url

--- scripts/renderFeed.jsx
+++ scripts/renderFeed.jsx
@@ -40,6 +40,18 @@
         }
     });
 
+    function getQueryStringParams(sParam){
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+
+        for (var i = 0; i < sURLVariables.length; i++){
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam){
+                return sParameterName[1];
+            }
+        }
+    }
+
     var FeedContainer = React.createClass({
         getInitialState: function(){
             return {
@@ -49,7 +61,13 @@
             };
         },
         componentDidMount: function(){
-            var username = "camilin87"
+            var username = getQueryStringParams("user");
+
+            if (!username){
+                window.location.href = "/";
+                return;
+            }
+
             var url = "https://twu-api.herokuapp.com/public/api/v1/feed/" + username;
             $.getJSON(url)
                 .done(function(json){
