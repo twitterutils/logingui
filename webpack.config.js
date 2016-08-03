2d32e80cab1de923a688de892fa356b5e4c6d43b
camilin87 <camilin87@gmail.com>
2016-08-03 15:37:52 -0400

    install babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,15 @@ var config = {
   output: {
     path: BUILD_DIR,
     filename: '/scripts/bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
   }
 };
 
