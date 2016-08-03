504ead86139ca032cb5f6d530fe61ba88114e486
camilin87 <camilin87@gmail.com>
2016-08-03 16:01:15 -0400

    change the client bundle infrastructure to accept multiple bundles

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,12 @@ var BUILD_DIR = path.resolve(__dirname, 'build/');
 var APP_DIR = path.resolve(__dirname, 'src/');
 
 var config = {
-  entry: APP_DIR + '/scripts/testBundle/index.jsx',
+  entry: {
+    testBundle: APP_DIR + '/scripts/testBundle/index.jsx'
+  },
   output: {
-    path: BUILD_DIR,
-    filename: '/scripts/testBundle.js'
+    path:  path.join(BUILD_DIR, "scripts/"),
+    filename: '[name].js'
   },
   module : {
     loaders : [
