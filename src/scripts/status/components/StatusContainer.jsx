89063558982a4cea4b296edcb1bb8c0c60dd10d7
camilin87 <camilin87@gmail.com>
2016-08-04 07:26:08 -0400

    refactor the status container to remove duplication

--- src/scripts/status/components/StatusContainer.jsx
+++ src/scripts/status/components/StatusContainer.jsx
@@ -2,15 +2,31 @@ import React from 'react';
 import SingleApiStatus from './SingleApiStatus.jsx';
 
 class StatusContainer extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            endpoints: [
+                {url: "/login/api/v1/status", name: "Login Api V1"},
+                {url: "/graph/api/v1/status", name: "Graph Api V1"},
+                {url: "/notifications/api/v1/status", name: "Notifications Api V1"},
+                {url: "/feed/api/v1/status", name: "Feed Api V1"},
+                {url: "/usernames/api/v1/status", name: "Usernames Api V1"},
+                {url: "/public/api/v1/status", name: "Public Api V1"}
+            ]
+        }
+    }
+
     render(){
+        var statusesElements = this.state.endpoints.map((e) => {
+            return (
+                <SingleApiStatus apiUrl={e.url} apiName={e.name} key={e.name}/>
+            )
+        })
+
         return (
             <div>
-                <SingleApiStatus apiUrl="/login/api/v1/status" apiName="Login Api V1" />
-                <SingleApiStatus apiUrl="/graph/api/v1/status" apiName="Graph Api V1" />
-                <SingleApiStatus apiUrl="/notifications/api/v1/status" apiName="Notifications Api V1" />
-                <SingleApiStatus apiUrl="/feed/api/v1/status" apiName="Feed Api V1" />
-                <SingleApiStatus apiUrl="/usernames/api/v1/status" apiName="Usernames Api V1" />
-                <SingleApiStatus apiUrl="/public/api/v1/status" apiName="Public Api V1" />
+                {statusesElements}
             </div>
         );
     }
