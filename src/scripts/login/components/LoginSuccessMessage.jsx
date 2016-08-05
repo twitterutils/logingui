81a29a803097fccca180e652f9d3b75dd97e4791
camilin87 <camilin87@gmail.com>
2016-08-04 22:44:50 -0400

    style the alert a lil bit better

--- src/scripts/login/components/LoginSuccessMessage.jsx
+++ src/scripts/login/components/LoginSuccessMessage.jsx
@@ -22,7 +22,12 @@ class LoginSuccessMessage extends React.Component {
         }
 
         return (
-            <h2>Login Successful</h2>
+            <div className="alert alert-success alert-dismissible" role="alert">
+                <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                Login Successful
+            </div>
         );
     }
 }
